8ebec84e0e63bedb2a983338c56a05b7f79c3bc5
Sam Byass <sam@whcuk.net>
2015-09-04 19:50:21 +0100

    Begin Improvement of /reboot

--- WhiteHorseCore/src/main/java/net/whcuk/whitehorsecraft/whitehorsecore/WhiteHorseCore.java
+++ WhiteHorseCore/src/main/java/net/whcuk/whitehorsecraft/whitehorsecore/WhiteHorseCore.java
@@ -56,18 +56,80 @@ public class WhiteHorseCore extends JavaPlugin implements ActionListener
 	}
 	public void actionPerformed(ActionEvent e) 
 	{
-		if (time == 40)
+		if (time == 60)
 		{
 			getServer().shutdown();
 		}
+		else if (time == 59)
+		{
+			getServer().broadcastMessage(ChatColor.RED+"Server Rebooting Now!");
+			timer.setDelay(1000);
+			time = 60;
+		}
+		else if (time == 58)
+		{
+			getServer().broadcastMessage(ChatColor.RED+"Server Rebooting in 1 Seconds!");
+			timer.setDelay(1000);
+			time = 59;
+		}
+		else if (time == 57)
+		{
+			getServer().broadcastMessage(ChatColor.RED+"Server Rebooting in 2 Seconds!");
+			timer.setDelay(1000);
+			time = 58;
+		}
+		else if (time == 56)
+		{
+			getServer().broadcastMessage(ChatColor.RED+"Server Rebooting in 3 Seconds!");
+			timer.setDelay(1000);
+			time = 57;
+		}
+		else if (time == 55)
+		{
+			getServer().broadcastMessage(ChatColor.RED+"Server Rebooting in 4 Seconds!");
+			timer.setDelay(1000);
+			time = 56;
+		}
+		else if (time == 54)
+		{
+			getServer().broadcastMessage(ChatColor.RED+"Server Rebooting in 5 Seconds!");
+			timer.setDelay(1000);
+			time = 55;
+		}
+		else if (time == 53)
+		{
+			getServer().broadcastMessage(ChatColor.RED+"Server Rebooting in 6 Seconds!");
+			timer.setDelay(1000);
+			time = 54;
+		}
+		else if (time == 52)
+		{
+			getServer().broadcastMessage(ChatColor.RED+"Server Rebooting in 7 Seconds!");
+			timer.setDelay(1000);
+			time = 53;
+		}
+		else if (time == 51)
+		{
+			getServer().broadcastMessage(ChatColor.RED+"Server Rebooting in 8 Seconds!");
+			timer.setDelay(1000);
+			time = 52;
+		}
+		else if (time == 50)
+		{
+			getServer().broadcastMessage(ChatColor.RED+"Server Rebooting in 9 Seconds!");
+			timer.setDelay(1000);
+			time = 51;
+		}
 		else if (time == 30)
 		{
-			
+			getServer().broadcastMessage(ChatColor.RED+"Server Rebooting in 20 Seconds!");
+			timer.setDelay(10000);
+			time = 40;
 		}
 		else if (time == 0)
 		{
 			getServer().broadcastMessage(ChatColor.RED+"Server Rebooting in 30 Seconds!");
-			timer.setDelay(20000);
+			timer.setDelay(10000);
 			time = 30;
 		}
 	}
